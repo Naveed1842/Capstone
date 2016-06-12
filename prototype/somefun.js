6dc7a60a4dce12052f1d4580aadbcfe54a841cb7
Nikhil Kishore <nk1877@nyu.edu>
2016-06-11 20:49:25 -0400

    update 3.2

---
+++ prototype/somefun.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+		$("#oneWay").click(function(){
+			$("#dropdown2").css("display","none");
+		});
+		$("#return").click(function(){
+			$("#dropdown2").css("display","block");
+		});
+		
+	})
